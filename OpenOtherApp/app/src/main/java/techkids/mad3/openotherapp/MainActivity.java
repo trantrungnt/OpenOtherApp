ac44b51521dafc9c3d081d622ab131283e16913e
Trần Trung <nguyentrantrung1986@gmail.com>
2016-07-23 20:52:46 +0700

    define openOtherApplication method (temp)

--- OpenOtherApp/app/src/main/java/techkids/mad3/openotherapp/MainActivity.java
+++ OpenOtherApp/app/src/main/java/techkids/mad3/openotherapp/MainActivity.java
@@ -2,12 +2,36 @@ package techkids.mad3.openotherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mButtonOpenFirst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponent();
+    }
+
+    private void initComponent() {
+        mButtonOpenFirst = (Button) this.findViewById(R.id.btnOpenFirst);
+        mButtonOpenFirst.setOnClickListener(this);
+    }
+
+    private void openOtherApplication() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.btnOpenFirst:
+                onBackPressed();
+                break;
+        }
     }
 }
