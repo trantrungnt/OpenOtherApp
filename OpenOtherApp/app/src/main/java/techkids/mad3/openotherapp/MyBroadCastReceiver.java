5711a04ba42db0b303edd08b761b9155d9b5b175
Trần Trung <nguyentrantrung1986@gmail.com>
2016-07-24 15:42:32 +0700

    config Mainifest and define MyBroadCastRecevier

---
+++ OpenOtherApp/app/src/main/java/techkids/mad3/openotherapp/MyBroadCastReceiver.java
@@ -0,0 +1,19 @@
+package techkids.mad3.openotherapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by TrungNT on 7/24/2016.
+ */
+public class MyBroadCastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.d("TAG", "Ban vua khoi dong lai may");
+        }
+
+    }
+}
